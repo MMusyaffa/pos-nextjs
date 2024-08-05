e12dab723b3d0dd1c29943e7029b079de719d8b7
Dimaspadma <dimaspadma@gmail.com>
2024-08-05 21:54:08 +0700

    bugfix: use mock data admin

--- pos-next/src/app/admin/categories/page.js
+++ pos-next/src/app/admin/categories/page.js
@@ -5,73 +5,73 @@ import AddCategories from "./addCategories";
 import DeleteCategories from "./deleteCategories";
 import UpdateCategories from "./updateCategories";
 
-// export const metadata = {
-//   title: "Categories List",
-// };
+import dummyData from "../../../data/dummy.json";
 
-async function fetchCategories()
-{
-    const res = await fetch ("http://localhost:4000/categories", {
-        cache: 'no-store', //untuk mengambil data pada setiap request
-    });
 
-    return res.json();
+async function fetchCategories() {
+    if (process.env.USE_MOCK) {
+        // todo: fetch from backend
+    }
+    else {
+        return dummyData.data.categories;
+    }
 }
 
 export default function CategoriesList() {
 
     //const categories = await getCategories();
     const [getCategories, setGetCategories] = useState([]);
-    
-    useEffect(()=> {
+
+    useEffect(() => {
         fetchCategories()
-            .then( function (res) {setGetCategories(res)
+            .then(function (res) {
+                setGetCategories(res)
             })
 
     }, []);
-    
+
 
     return (
-            <div className="py-3 px-10 w-full ml-36">
+        <div className="py-3 px-10 w-full ml-36">
             <h2 className="text-3xl font-bold sm:text-4l">Dhadhu Caffee Dashboard Categories</h2>
             <h2 className="py-4 text-l font-medium">Welcome Admin 1 </h2>
-                <div className="py-2 pt-8">
-                    <AddCategories/>
-                </div>
-                
-                <table className="table w-full">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Image</th>
-                            <th>Categories Name</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    
-                    <tbody>
-                        {getCategories.map((categories, index) => 
-                        (
-                            <tr key = {categories.id}>
-                                <td>
-                                    {index + 1}
-                                    </td>
-                                <td>
-                                    <img src={categories.image} style={{ maxWidth: '100px' }}/>
-                                </td>
-                                <td>
-                                    {categories.title}
-                                </td>
-                                <td className="flex">
-                                    <div className="mr-1">
-                                        <UpdateCategories {...categories}/>
-                                    </div>
-                                        <DeleteCategories {...categories}/>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+            <div className="py-2 pt-8">
+                <AddCategories />
             </div>
+
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Image</th>
+                        <th>Categories Name</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {getCategories.map((categories, index) =>
+                    (
+                        <tr key={categories.id}>
+                            <td>
+                                {index + 1}
+                            </td>
+                            <td>
+                                <img src={categories.image} style={{ maxWidth: '100px' }} />
+                            </td>
+                            <td>
+                                {categories.name}
+                            </td>
+                            <td className="flex">
+                                <div className="mr-1">
+                                    <UpdateCategories {...categories} />
+                                </div>
+                                <DeleteCategories {...categories} />
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
--- pos-next/src/app/admin/products/page.js
+++ pos-next/src/app/admin/products/page.js
@@ -5,22 +5,12 @@ import AddProduct from "./addProduct";
 import DeleteProduct from "./deleteProduct";
 import UpdateProduct from "./updateProduct";
 
-// import dummyData from "../../../../../data/dummy.json";
+import dummyData from "../../../data/dummy.json";
 
-// export const metadata = {
-//     title: "Product List",
-//   };
 
-const useApi = true;
-
-async function fetchProducts()
-{
-    if (useApi) {
-        const res = await fetch ("http://localhost:5000/products", {
-            cache: 'no-store', //untuk mengambil data pada setiap request    
-    });
-
-    return res.json();
+async function fetchProducts() {
+    if (process.env.USE_MOCK) {
+        // todo: fetch from backend
     }
     else {
         return dummyData.data.products;
@@ -29,77 +19,77 @@ async function fetchProducts()
 
 const formatRupiah = (angka, prefix) => {
 
-        var number_string = angka.replace(/[^,\d]/g, '').toString(),
+    var number_string = angka.replace(/[^,\d]/g, '').toString(),
         split = number_string.split(','),
         sisa = split[0].length % 3,
         rupiah = split[0].substr(0, sisa),
         ribuan = split[0].substr(sisa).match(/\d{3}/gi);
 
-        if (ribuan) {
+    if (ribuan) {
         var separator = sisa ? '.' : '';
         rupiah += separator + ribuan.join('.');
-        }
-
-        rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-        return prefix == undefined ? rupiah : rupiah ? 'Rp.' + rupiah : '';
     }
 
-export default function ProductsList() 
-{
+    rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+    return prefix == undefined ? rupiah : rupiah ? 'Rp.' + rupiah : '';
+}
+
+export default function ProductsList() {
     // const products = await getProducts();
     const [getProducts, setGetProducts] = useState([]);
 
-    useEffect(()=> {
+    useEffect(() => {
         fetchProducts()
-            .then( function (tit) {setGetProducts(tit)
+            .then(function (tit) {
+                setGetProducts(tit)
             })
 
     }, []);
-    
+
     return (
         // <div className="flex">
         //     <SideBarAdmin />
 
-            <div className="py-3 px-10 w-full ml-36">
+        <div className="py-3 px-10 w-full ml-36">
             <h2 className="text-3xl font-bold sm:text-4l">Dhadhu Caffee Dashboard</h2>
             <h2 className="py-4 text-l font-medium">Welcome Admin 1 </h2>
-                <div className="py-2 pt-8">
-                    <AddProduct/>
-                </div>
-                
-                <table className="table w-full">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Image</th>
-                            <th>Product Name</th>
-                            <th>Categories</th>
-                            <th>Price</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {getProducts.map((product, index) => 
-                        (
-                            <tr key = {product.id}>
-                                <td>{index + 1}</td>
-                                <td>
-                                    <img src={product.image} style={{ maxWidth: '90px' }}/>
-                                </td>
-                                <td>{product.title}</td>
-                                <td>{product.categories}</td>
-                                <td>{formatRupiah(product.price.toString(), 'Rp.')}</td>
-                                <td className="flex">
-                                    <div className="mr-1">
-                                        <UpdateProduct {...product}/>
-                                    </div>
-                                        <DeleteProduct {...product}/>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+            <div className="py-2 pt-8">
+                <AddProduct />
             </div>
+
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Image</th>
+                        <th>Product Name</th>
+                        <th>Categories</th>
+                        <th>Price</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {getProducts.map((product, index) =>
+                    (
+                        <tr key={product.id}>
+                            <td>{index + 1}</td>
+                            <td>
+                                <img src={product.image} style={{ maxWidth: '90px' }} />
+                            </td>
+                            <td>{product.title}</td>
+                            <td>{product.categories}</td>
+                            <td>{formatRupiah(product.price.toString(), 'Rp.')}</td>
+                            <td className="flex">
+                                <div className="mr-1">
+                                    <UpdateProduct {...product} />
+                                </div>
+                                <DeleteProduct {...product} />
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
         //</div>
     );
 } 
